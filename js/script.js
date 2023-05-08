06473057b9747b6319e972a156109be58d6b30de
immortalR012 <r.bessmertny@yandex.ru>
2023-05-09 01:04:39 +0300

    comit ORGANAISATION

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("burger-button").addEventListener("click", function () {
+    document.getElementById("modal-windou").classList.add("open")
+}
+)
+
+document.getElementById("modal-close").addEventListener("click", function () {
+    document.getElementById("modal-windou").classList.remove("open")
+}
+)
